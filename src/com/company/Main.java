fab4ccc76a22ed7ad829bfb2f52e4494ba0f121b
yupeng <ylu1100@bths.edu>
2018-10-15 21:02:35 -0400

    guessgame1 name + guesses

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
 	double i = Math.random();
-	int x = (int)(i*11);
+	int x = (int)(i*100);
+	int guesses = 0;
 	Scanner input = new Scanner(System.in);
-	System.out.println("Guess my number between 1 to 10");
+	System.out.println("What's your name?");
+	String name = input.nextLine();
+	System.out.println("Hey "+ name + " guess my number between 1 to 100");
 	while (!(input.nextInt() == x)){
-	    System.out.println("Guess again");// write your code here
+	    System.out.println("Guess again, "+ name);
+	    guesses++;// write your code here
     }
-    System.out.println("You are correct! The number was " + x);
+    System.out.println(name +" is correct! The number was " + x);
+	System.out.println("It took "+ name+" " + guesses+ " tries.");
     }
 }
